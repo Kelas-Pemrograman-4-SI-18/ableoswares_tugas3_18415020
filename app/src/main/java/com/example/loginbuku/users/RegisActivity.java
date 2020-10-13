26ce5a4991ca0246e9230e56e37882934be014b7
10-Ableo <2000ableo@Gmail.com>
2020-10-13 22:11:22 +0700

    LoginBuku

---
+++ app/src/main/java/com/example/loginbuku/users/RegisActivity.java
@@ -0,0 +1,33 @@
+package com.example.loginbuku.users;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+
+
+
+import com.example.loginbuku.R;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+public class RegisActivity extends AppCompatActivity {
+    Button btnPindah;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_regis);
+        getSupportActionBar().hide();
+
+        btnPindah = (Button) findViewById(R.id.btnPindah);
+
+        btnPindah.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(RegisActivity.this, LoginActivity.class);
+                startActivity(i);
+                finish();
+            }
+        });
+    }
+}
